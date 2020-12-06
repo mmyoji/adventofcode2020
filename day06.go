7f677b84c0b2db117b78d6e927a921b85ae465c7
mmyoji <mmyoji@gmail.com>
2020-12-06 15:30:16 +0900

    Day 6: Cosme

--- day06.go
+++ day06.go
@@ -7,24 +7,50 @@ func Day6() {
 	lines, err := getLines("inputs/day06.txt")
 	fatal(err)
 
-	answers := make(map[rune]bool)
-	count := 0
+	g := &groups{count: 0}
+	g.setItems(lines)
+	g.countTotalAnswers()
 
+	fmt.Printf("answer: %d\n", g.count)
+	// 6633
+}
+
+// groups is just a namaspace
+type groups struct {
+	items map[int]string
+	count int
+}
+
+func (g *groups) setItems(lines []string) {
+	g.items = make(map[int]string)
+	groupIdx := 0
 	for _, line := range lines {
 		if line == "" {
-			count += len(answers)
-			answers = make(map[rune]bool)
+			groupIdx++
 			continue
 		}
 
-		for _, c := range line {
-			answers[c] = true
+		_, ok := g.items[groupIdx]
+		if !ok {
+			g.items[groupIdx] = line
+		} else {
+			g.items[groupIdx] += line
 		}
 	}
+}
 
-	// Add the last group
-	count += len(answers)
+func (g *groups) countTotalAnswers() {
+	for _, line := range g.items {
+		g.count += g.countAnswers(line)
+	}
+}
 
-	fmt.Printf("answer: %d\n", count)
-	// 6633
+func (g *groups) countAnswers(line string) int {
+	answers := make(map[rune]bool)
+
+	for _, c := range line {
+		answers[c] = true
+	}
+
+	return len(answers)
 }
