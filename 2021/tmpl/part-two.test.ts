407589b505c27fccf3588c569387fd34e9df0f18
mmyoji <mmyoji@gmail.com>
2021-12-08 21:12:27 +0900

    fix: fix tmpl

--- 2021/tmpl/part-two.test.ts
+++ 2021/tmpl/part-two.test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
 
 import { fetchData } from "./input.ts";
-import { run } from "./part-one.ts";
+import { run } from "./part-two.ts";
 
 Deno.test("run", async () => {
   const data = await fetchData("./sample.txt");
