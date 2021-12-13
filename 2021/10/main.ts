27ba1f84eb23c80f3a543d717e166f92edeada03
mmyoji <mmyoji@gmail.com>
2021-12-13 20:46:18 +0900

    2021-10 part 2

--- 2021/10/main.ts
+++ 2021/10/main.ts
@@ -4,4 +4,4 @@ import { run as partTwo } from "./part-two.ts";
 
 const data = await fetchData("./input.txt");
 console.log("Part 1:", partOne(data)); // 436497
-console.log("Part 2:", partTwo(data));
+console.log("Part 2:", partTwo(data)); // 2377613374
--- 2021/10/part-two.ts
+++ 2021/10/part-two.ts
@@ -1,4 +1,89 @@
-export function run(data: unknown) {
-  // TODO: impl
-  return 2;
+type OpenChunk = "(" | "[" | "{" | "<";
+type CloseChunk = ")" | "]" | "}" | ">";
+
+const OPEN_CHUNKS = ["(", "[", "{", "<"];
+const CLOSE_CHUNKS = [")", "]", "}", ">"];
+
+const CHUNKS: Record<OpenChunk, CloseChunk> = {
+  "(": ")",
+  "[": "]",
+  "{": "}",
+  "<": ">",
+};
+
+const SCORES: Record<CloseChunk, number> = {
+  ")": 1,
+  "]": 2,
+  "}": 3,
+  ">": 4,
+};
+
+function isOpenChunk(s: string): s is OpenChunk {
+  return !!s && OPEN_CHUNKS.includes(s);
+}
+
+function isCloseChunk(s: string): s is CloseChunk {
+  return !!s && CLOSE_CHUNKS.includes(s);
+}
+
+function validate(chunk: CloseChunk, chunks: CloseChunk[]): boolean {
+  const arr = [...chunks];
+  const last = arr.pop();
+  if (last == chunk) {
+    chunks.pop();
+    return true;
+  }
+
+  return false;
+}
+
+function findIncomplete(line: string): number {
+  const chunks: CloseChunk[] = [];
+
+  for (const char of line) {
+    if (isOpenChunk(char)) {
+      chunks.push(CHUNKS[char]);
+      continue;
+    }
+
+    if (isCloseChunk(char)) {
+      if (validate(char, chunks)) {
+        continue;
+      }
+
+      return 0;
+    }
+
+    throw new Error(`Invalid char detected: ${char}`);
+  }
+
+  // Incomplete
+  if (chunks.length) {
+    let total = 0;
+    while (chunks.length) {
+      const chunk = chunks.pop();
+      if (chunk == null) break;
+
+      total = total * 5 + SCORES[chunk];
+    }
+
+    return total;
+  }
+
+  return 0;
+}
+
+export function run(data: string[]) {
+  const scores: number[] = [];
+
+  for (const line of data) {
+    const score = findIncomplete(line);
+    if (score > 0) {
+      scores.push(score);
+    }
+  }
+
+  const sortedScores = [...scores].sort((a, b) => a - b);
+
+  return sortedScores[Math.floor(scores.length / 2)];
 }
