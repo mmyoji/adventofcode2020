6ec39e446f78c5a9af2d5b4ffc686c2c45769b5a
mmyoji <mmyoji@gmail.com>
2021-12-15 19:53:01 +0900

    2021-11 Part 2 (Wrong Answer)

--- 2021/11/main.ts
+++ 2021/11/main.ts
@@ -4,4 +4,4 @@ import { run as partTwo } from "./part-two.ts";
 
 const data = await fetchData("./input.txt");
 console.log("Part 1:", partOne(data)); // 1585
-console.log("Part 2:", partTwo(data));
+console.log("Part 2:", partTwo(data)); // 282
